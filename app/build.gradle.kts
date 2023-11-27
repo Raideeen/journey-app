ef18bedacc80f179e4e48dd16755c5dcc884cce7
Adrien Djebar <adrien.djebar@proton.me>
2023-11-27 23:43:57 +0100

    Added sidebar functionality working with FragmentContainerView.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,10 @@ dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.annotation:annotation:1.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -56,4 +58,10 @@ dependencies {
 
     // Coil library for loading images
     implementation("io.coil-kt:coil:2.5.0")
+
+    // Material Design
+    implementation("com.google.android.material:material:1.10.0")
+
+    // Rounded Image View
+    implementation("com.makeramen:roundedimageview:2.3.0")
 }
