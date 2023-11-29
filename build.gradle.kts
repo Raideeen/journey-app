942ffef58599a5411cf01680882c1563d0ded683
Adrien Djebar <adrien.djebar@proton.me>
2023-11-29 14:59:52 +0100

    Changed gradle build to 8.1.4.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0-rc03" apply false
+    id("com.android.application") version "8.1.4" apply false
     id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
 
@@ -11,7 +11,7 @@ buildscript {
         mavenCentral()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:8.2.0-rc03")
+        classpath("com.android.tools.build:gradle:8.1.4")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.7.5")
     }
